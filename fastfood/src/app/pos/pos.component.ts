941b32cb3725cb22ed2d976ae664d2eb6be9eb98
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-09-17 17:16:13 -0300

    pos hecho

--- fastfood/src/app/pos/pos.component.ts
+++ fastfood/src/app/pos/pos.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PedidoService } from '../pedido-service';
+import { Pedido } from '../../pedido';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pos.component.html',
   styleUrl: './pos.component.css'
 })
 export class PosComponent {
+  pedidoService = inject(PedidoService)
+  nuevoPedido : Pedido = new Pedido()
 
+  enviarPedido(){
+    this,this.nuevoPedido.number = Math.floor(Math.random() * 1000) + 1 
+    this.pedidoService.addPedidoAIngresar(this.nuevoPedido)
+    this.nuevoPedido = new Pedido()
+  }
 }
